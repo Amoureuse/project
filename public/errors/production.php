be2a7014a34bcf498ff4bd9ad224fe396e5c8161
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-16 19:04:29 +0200

    Add files via upload

---
+++ public/errors/production.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Ошибка</title>
+    </head>
+    <body>
+        <h3>Произошла ошибка</h3>
+    </body>
+</html>
