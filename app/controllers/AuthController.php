7a9df6b8401b9295bf26fb9fa409f40fca87284a
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-18 18:57:59 +0200

    Update AuthController.php

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,83 +1,90 @@
+
 <?php
 namespace app\controllers;
-use app\controllers\Controller;
+use app\controllers\AppController;
 
 
-class AuthController extends Controller{
+class AuthController extends AppController{
+    protected $model;
     
+    public function __construct() {
+        $this->model = new \app\models\UserModel();
+    }
+
     public function login(){
-       $this->check();
-        $this->view('auth', $this->data);
+        if(!empty($_POST)){
+            $email = $_POST['email'];
+            $user = $this->model->read(['email' => $email]);
+            if(!$user){
+               redirect('/login') ;
+            }
+            if(password_verify($_POST['pass'], $user['pass'])){
+                $_SESSION['logged_user'] = $user['id'];
+                redirect('/');
+            }else{
+                redirect('/login'); 
+            }
+        }else{
+        $this->view('auth', $data=[]);
+        }
     }
       public function register(){
-        $this->check();
-        $this->view('register', $this->data);
-        
+        if(!empty($_POST)){
+            $errors = $this->checkRegister();
+            var_dump($errors);
+            if($errors !== true){
+                $oldData = [
+                    'email' => $_POST['email'],
+                    'username'=>$_POST['username']
+                ];
+            redirect('/register');
+            }else{
+            $data = $_POST;
+            $userId = $this->model->create($data);
+            $_SESSION['logged_user'] = $userId;
+            redirect('/');
+          }
+        }else{
+          $this->view('register', $data=[]);  
+        }
     }
+    
     public function logout(){
         unset($_SESSION['logged_user']);
-        header("Location: http://myproject/");
+        redirect('/');
     }
-    public function check(){
+    public function checkRegister(){
         if(isset($_POST['submit_reg'])){
             $errors = array(); 
+            $email = $_POST['email'];
+            $user = $this->model->read(['email' => $email]);
+            var_dump($user);
+            if(isset($user['email'])){
+                $errors[] = 'Пользователь с таким email существует';
+            }
+            if(isset($user['username']) && $user['username'] == $_POST['username']){
+                $errors[] = 'Пользователь с таким именем существует';
+            }
             if(trim($_POST['username']) == '' || (!ctype_alnum($_POST['username']))){
                  $errors[] = 'Некорректный логин!';
             }
+            
             if(trim($_POST['email']) == ''){
                  $errors[] = 'Введите email!';
             }
+            
             if($_POST['pass'] == ''){
                  $errors[] = 'Введите пароль!';
             }
             if($_POST['pass_2'] != $_POST['pass']){
                  $errors[] = 'Пароли не совпадают!';
             }
-            if(empty($errors)){
-            $this->writeUser();
-                echo'<div style ="color:green;">Вы успешно зарегистрированы!'
-            . '<a href="http://myproject/auth/login">Авторизуйтесь</a></div><hr>';
-            }else{
-                echo'<div style ="color:red;">' . array_shift($errors) .'</div><hr>';
-            }
-          }
-        if(isset($_POST['submit_log'])){
-            $user = $this->getUser();
-            if(isset($user)){
-                if(password_verify($_POST['pass'], $user['pass'])){
-                    $_SESSION['logged_user'] = $user;
-                    header("Location: http://myproject/");
-                }else{
-                    $errors[] = 'Неверный пароль!';
-                } 
-            }else{
-                $errors[] = 'Пользователя не существует!';
-            }
-            if(!empty($errors)){
-                echo'<div style ="color:red;">' . array_shift($errors) .'</div><hr>';
-            }
-        }  
-        
+        }
+        if(empty($errors)){
+                return true;
+        }else{
+                return $errors;
+        }
     }
     
-    public function writeUser(){
-        
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $password = password_hash($_POST['pass'],PASSWORD_DEFAULT);
-        $stmt = $this->connect->prepare("INSERT INTO users (username,email, pass) VALUES (? ,? ,?)");
-        $stmt->bind_param('sss',$username, $email, $password);
-        $stmt->execute();
-        $result = $stmt->insert_id;
-        return true;
-    }
-    public function getUser(){
-        $email = $_POST['email'];
-        $stmt = $this->connect->prepare('SELECT * FROM users WHERE email = ?');
-        $stmt->bind_param('s', $email);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $data = $result->fetch_assoc();
-        return $data;
-    }
 }
