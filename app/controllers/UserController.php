1b96cc2b3e53b036abe3e0f0e1d3120a2f95a3b2
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-27 20:32:31 +0200

    Add files via upload

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -8,8 +8,13 @@ class UserController extends AppController {
     
     public function __construct($route) {
         parent::__construct($route);
-        if(!isset($_SESSION['logged'])){
+        $model= new UserModel();
+        if (!(\project\Auth::userID())) {
+            memUrl();
             redirect('/login');
+        }
+        if (isset($_SESSION['url'])) {
+            unset($_SESSION['url']);
         }
     }
 
@@ -22,30 +27,32 @@ class UserController extends AppController {
     }
     
     public function edit(){
-        $model= new UserModel();
+        
         $user = \project\Auth::getUser();
-        if(!empty($_POST)){
+        if (!empty($_POST)) {
             $errors = [];
-            if($errors){
-                $oldData = [
-                'username'=>$_POST['username'],
+            if ($errors) {
+                $data = [
+                    'username'=>$_POST['username'],
                 ];
-            splashMessage($errors,$oldData);
+                oldData($data);
+                splashMessage($errors);
             }
-        $fileType = substr($_FILES['path']['name'], strrpos($_FILES['path']['name'], '.')+1);
-        $file = "images/avatar/". md5($user['id']). '.' . $fileType;
-        move_uploaded_file($_FILES['path']['tmp_name'], $file);
-        $username = isset($_POST['username'])?$_POST['username']:$user['username'];
-        $password = isset($_POST['pass'])?password_hash($_POST['pass'], PASSWORD_DEFAULT):$user['pass'];
-            if($fileType == 'png'){
-            $avatar = md5($user['id']).'.png';
-            }else{
-            $avatar = md5($user['id']).'.jpg';
+            if (!empty($_FILES['path']['name'])) {
+                $fileType = pathinfo($_FILES['path']['name']);
+                $fileType = $fileType['extension'];
+                $file = "images/avatar/". md5($user['id']). '.' . $fileType;
+                move_uploaded_file($_FILES['path']['tmp_name'], $file);
+                $avatar = md5($user['id']). '.' . $fileType;
+            } else {
+                $avatar = null;
             }
+            $username = (!empty($_POST['username'])) ? $_POST['username'] : $user['username'];
+            $password = (!empty($_POST['pass'])) ? password_hash($_POST['pass'], PASSWORD_DEFAULT) : $user['pass'];
         }
-        if(isset($_POST['submit'])){
-        $model->update($user['id'],$username,$password,$avatar);
-        redirect('/user');
+        if (isset($_POST['submit'])) {
+            $model->update($user['id'],$username,$password,$avatar);
+            redirect('/user');
         }
         $data=[
             'user'=>$user,
