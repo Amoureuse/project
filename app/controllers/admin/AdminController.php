06294b90d2e15e9bb78fb3026270eb2eaa3ac3e0
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-28 20:45:36 +0200

    Add files via upload

--- app/controllers/admin/AdminController.php
+++ app/controllers/admin/AdminController.php
@@ -1,56 +1,53 @@
 <?php 
+
 namespace app\controllers\admin;
 
-Class AdminController extends \app\controllers\AppController{
-    
-    public function __construct($route) {
+use app\controllers\AppController;
+use app\models\ItemModel;
+use project\Auth;
+
+Class AdminController extends AppController
+{
+
+    public function __construct($route)
+    {
         parent::__construct($route);
-        if(!\project\Auth::isAdmin()){
+        if (!Auth::isAdmin()) {
             redirect('/');
         }
+        $this->model = new ItemModel();
     }
 
-    public function index(){
-        $model = new \app\models\ItemModel();
-        $items = $model->get_arr_items();
+    public function index()
+    {
+        $items = $this->model->get_arr_items();
         $data = [
             'items'=> $items,
             ];    
         $this->view('admin/index',$data);
     }
     
-    public function add(){
-        if(isset($_POST['add_product'])){
-            if($_POST['name'] != ''){
-            $this->addProduct();
+    public function add()
+    {
+        if (isset($_POST['add_product'])) {
+            if ($_POST['name'] != '') {
+                $this->addProduct();
             }    
         }
         $this->view('admin/add', $data=[]);
     }
  
-    public function edit(){
-        if(isset($_POST['edit_product'])){
-            if($_POST['name'] != ''){
-            $this->updateProduct();
-            }    
+    public function edit()
+    {
+        if (isset($_POST['edit_product'])) {
+            if ($_POST['name'] != '') {
+                $this->updateProduct();
+            }   
         }
         $this->view('admin/edit', $data=[]);
     }
-    public function addProduct($name,$description,$price,$stock,$disc,$image=null){
-        $stmt = $this->connect->prepare("INSERT INTO goods (name,description, price, stock,disc,image) VALUES (? ,? ,?,?,?,?)");
-        $stmt->bind_param('ssdiis',$name,$description,$price,$stock,$disc,$image);
-        $stmt->execute();
-        $result = $stmt->insert_id;
-        return $result; 
-    }
-    
-    public function updateProduct($id,$name,$description,$price,$stock,$disc,$image=null){
-        $stmt = $this->connect->prepare("UPDATE goods SET name = ?,description = ?, price = ?, stock = ?,disc = ?,image = ? WHERE id = $id");
-        $stmt->bind_param('ssdiis',$name,$description,$price,$stock,$disc,$image);
-        $stmt->execute();
-        return true; 
-    }
+
 }   
 
-     ?>
+
 
