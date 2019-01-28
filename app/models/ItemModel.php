a95e8c64ee1d757d3eff6733b6d7fd2f9e1d411f
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-28 20:49:03 +0200

    Add files via upload

--- app/models/ItemModel.php
+++ app/models/ItemModel.php
@@ -1,26 +1,39 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace app\models;
 
+use app\controllers\Item;
 
-class ItemModel extends Model{
+class ItemModel extends Model
+{
     protected $table = 'items';
     
-   public function get_arr_items(){
-            $res = $this->connect->query("SELECT * FROM goods");
-            $items =$res->fetch_all(MYSQLI_ASSOC);
-            $itemsDataObj = [];
-    
+    public function get_arr_items()
+    {
+        $res = $this->connect->query("SELECT * FROM goods");
+        $items =$res->fetch_all(MYSQLI_ASSOC);
+        $itemsDataObj = [];
         foreach ($items as $item) {
-            $itemsDataObj[] = new \app\controllers\Item($item);
-            }
-            return $itemsDataObj;
+            $itemsDataObj[] = new Item($item);
+        }
+        return $itemsDataObj;
+    }
+    
+    public function addProduct($name, $description, $price, $stock, $disc, $image=null)
+    {
+        $stmt = $this->connect->prepare("INSERT INTO goods (name, description, price, stock, disc, image) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param('ssdiis', $name, $description, $price, $stock, $disc, $image);
+        $stmt->execute();
+        $result = $stmt->insert_id;
+        return $result; 
+    }
+    
+    public function updateProduct($id, $name, $description, $price, $stock, $disc, $image=null)
+    {
+        $stmt = $this->connect->prepare("UPDATE goods SET name = ?, description = ?, price = ?, stock = ?, disc = ?, image = ? WHERE id = $id");
+        $stmt->bind_param('ssdiis', $name, $description, $price, $stock, $disc, $image);
+        $stmt->execute();
+        return true; 
     }
     
     
