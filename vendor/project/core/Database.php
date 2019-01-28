a326106572d3e5a923cebd627205478b37554197
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-28 21:00:40 +0200

    Add files via upload

--- vendor/project/core/Database.php
+++ vendor/project/core/Database.php
@@ -1,25 +1,27 @@
 <?php 
+
 namespace project;
 
-class Database{
-	public $connection;
-	public static $_instance;
+class Database
+{
+    public $connection;
+    public static $_instance;
 
-	 public static function getInstance($config = []) {
-        if(null === static::$_instance) {
+    public static function getInstance($config = [])
+    {
+        if (static::$_instance === null) {
             static::$_instance = new static($config);
         }
         return static::$_instance;
     }
 
-    private function __construct($config){
+    private function __construct($config)
+    {
         $this->connection = new \mysqli($config['host'], $config['user'], $config['password'], $config['db']);
         $this->connection->set_charset("utf8");
-        if ($this->connection->connect_errno)
-        {
+        if ($this->connection->connect_errno) {
             printf($this->connection->connect_error);
             die();
         }
     }
-    }
-?>
+}
