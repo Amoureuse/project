838742c8fb6cdb73bc4af7ec637c061aa5e79974
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-28 16:58:27 +0200

    Add files via upload

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,14 +1,17 @@
 <?php
 
-
 namespace app\controllers;
+
 use app\models\UserModel;
 
-class UserController extends AppController {
+class UserController extends AppController
+{
+    use TValidation;
     
-    public function __construct($route) {
+    public function __construct($route)
+    {
         parent::__construct($route);
-        $model= new UserModel();
+        $this->model= new UserModel();
         if (!(\project\Auth::userID())) {
             memUrl();
             redirect('/login');
@@ -18,7 +21,8 @@ class UserController extends AppController {
         }
     }
 
-    public function index(){
+    public function index()
+    {
         $user = \project\Auth::getUser();
         $data=[
             'user'=>$user,
@@ -26,8 +30,8 @@ class UserController extends AppController {
         $this->set($data);
     }
     
-    public function edit(){
-        
+    public function edit()
+    {
         $user = \project\Auth::getUser();
         if (!empty($_POST)) {
             $errors = [];
@@ -39,11 +43,16 @@ class UserController extends AppController {
                 splashMessage($errors);
             }
             if (!empty($_FILES['path']['name'])) {
+                if ($this->filesValid() !== true) {
+                    splashMessage($this->filesValid(),'alert-danger');
+                    redirect('/user/edit');
+                }
                 $fileType = pathinfo($_FILES['path']['name']);
                 $fileType = $fileType['extension'];
                 $file = "images/avatar/". md5($user['id']). '.' . $fileType;
                 move_uploaded_file($_FILES['path']['tmp_name'], $file);
                 $avatar = md5($user['id']). '.' . $fileType;
+                splashMessage('Файл успешно загружен', 'alert-success');
             } else {
                 $avatar = null;
             }
@@ -51,8 +60,8 @@ class UserController extends AppController {
             $password = (!empty($_POST['pass'])) ? password_hash($_POST['pass'], PASSWORD_DEFAULT) : $user['pass'];
         }
         if (isset($_POST['submit'])) {
-            $model->update($user['id'],$username,$password,$avatar);
-            redirect('/user');
+            $this->model->update($user['id'], $username, $password, $avatar);
+            
         }
         $data=[
             'user'=>$user,
