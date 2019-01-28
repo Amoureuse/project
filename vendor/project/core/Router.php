cfcb6ee8fd29ff7a35af8555e70aa8e8f0814493
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-28 21:12:27 +0200

    Add files via upload

--- vendor/project/core/Router.php
+++ vendor/project/core/Router.php
@@ -1,61 +1,98 @@
 <?php
 
-
 namespace project;
 
-class Router {
-   public static $routes = [];
-   public static $route = [];
+class Router
+{
+    public static $routes = [];
+    public static $route = [];
    
-   public static function add($regexp, $route = []){
-       $regexp = '#^'. $regexp . '$#';
-       self::$routes[$regexp] = $route;
-   }
-   public static function getRoutes(){
-       return self::$routes;
-   }
-    public static function getRoute(){
-       return self::$route;
-   }
-    public static function matchRoute($url){ 
-        foreach (self::$routes as $pattern => $route){
-            if(preg_match($pattern, $url, $matches)){
-                if(!isset($route['prefix'])){
+    public static function add($regexp, $route = [])
+    {
+        $regexp = '#^'. $regexp . '$#';
+        self::$routes[$regexp] = $route;
+    }
+    
+    public static function getRoutes()
+    {
+        return self::$routes;
+    }
+    
+    public static function getRoute()
+    {
+        return self::$route;
+    }
+
+    public static function matchRoute($url)
+    { 
+        foreach (self::$routes as $pattern => $route) {
+            if (preg_match($pattern, $url, $matches)) {
+                if (!isset($route['prefix'])) {
                     $route['prefix'] = '';
-                }else{
+                } else {
                     $route['prefix'] .= '\\';
                 }
-               self::$route = $route;
-               return true;
+                self::$route = $route;
+                return true;
             }
-        }return false;
-   }
-   public static function dispatch($url){
-       $url = self::removeQueryString($url);
-       if(self::matchRoute($url)){
-           $controller = 'app\controllers\\' . self::$route['prefix'] . 
-           self::$route['controller'];
-           if(class_exists($controller)){
-               $cObj = new $controller(self::$route);
-               $action = self::$route['action'];
-               $cObj->$action();
-               $cObj->getView();
-           }else{
-               throw new \Exception("Контроллер $controller не найден", 404);
-           }   
-       }else{
-           throw new \Exception("Страница не найдена", 404); 
-       }
-   }
+        }
+        return false;
+    }
    
-   protected static function removeQueryString($url){ 
-       if($url){
-           $params = explode('&', $url, 2);
-           if(false === strpos($params[0], '=')){
-               return rtrim($params[0],'/');
-           }else{
-               return '';
-           }
-       }
-   }
+    public static function dispatch($url)
+    {
+        $url = self::removeQueryString($url);
+        if (self::matchRoute($url)) {
+            $controller = 'app\controllers\\' . self::$route['prefix'] . self::$route['controller'];
+            if (class_exists($controller)) {
+                $cObj = new $controller(self::$route);
+                $action = self::$route['action'];
+                $cObj->$action();
+                $cObj->getView();
+            } else {
+                throw new \Exception("Контроллер $controller не найден", 404);
+            }   
+        } else {
+            throw new \Exception("Страница не найдена", 404); 
+        }
+    }
+   
+    protected static function removeQueryString($url)
+    { 
+        if ($url) {
+            $params = explode('&', $url, 2);
+            if (false === strpos($params[0], '=')) {
+                return rtrim($params[0],'/');
+            } else {
+                return '';
+            }
+        }
+    }
+    //возврат url по имени маршрута;
+    public static function routeName($name)
+    {
+        $routes = [
+	    ['name'=>'home','url' => '', 'do'=>'Home/index'],
+	    ['name'=>'login','url' => '/auth/login', 'do'=>'Auth/login'],
+	    ['name'=>'register','url' => '/auth/register', 'do'=>'Auth/register']
+	    ];
+        foreach ($routes as $route) {
+	    if ($route['name']==$name) {
+                return $newRoute = $route['url'];
+	    }
+        }
+    }
+    // присвоение гет параметров к маршруту;
+    public static function route($name, $params=[])
+    {
+        $nameRoute = routeName($name) . '?';
+	if ($params == []) {
+            return rtrim($nameRoute, '?');//если параметров нет, обрезаем "?" и возвращаем url
+	}
+        foreach ($params as $key => $value) {
+	    $nameRoute .= $key . '=' . $value . '&';//добавляем указанные параметры к маршруту;
+        }
+        return rtrim($nameRoute, '&');//возвращаем url с параметрами, обрезая последний амперсант &;
+    }
+    
 }
