6a3183766bad6b05a67571939392a845d575b906
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-28 20:53:44 +0200

    Add files via upload

--- vendor/project/core/App.php
+++ vendor/project/core/App.php
@@ -1,20 +1,14 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace project;
 
-
-class App {
-    public function __construct() {
+class App
+{
+    public function __construct()
+    {
         $query = trim($_SERVER['QUERY_STRING'],'/');
         session_start();
         new ErrorHandler();
-        debug(Router::dispatch($query));
-        
+        Router::dispatch($query);     
     }
 }
