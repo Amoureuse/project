23a668dc20008694ae8854639ecccf8ffdb062cf
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-30 19:58:31 +0200

    Add files via upload

--- vendor/project/core/function.php
+++ vendor/project/core/function.php
@@ -1,78 +1,65 @@
 <?php
-function memUrl(){
-    $_SESSION['url'] = $_SERVER['REQUEST_URI'];    
-}
-//возврат url по имени маршрута;
-function routeName($name){
-    $routes = [
-	['name'=>'home','url' => '', 'do'=>'Home/index'],
-	['name'=>'login','url' => '/auth/login', 'do'=>'Auth/login'],
-	['name'=>'register','url' => '/auth/register', 'do'=>'Auth/register']
-	];
-    foreach ($routes as $route) {
-	if($route['name']==$name){
-            return $newRoute = $route['url'];
-	}
-    }
-}
-// присвоение гет параметров к маршруту;
-function route($name,$params=[]){
-    $nameRoute = routeName($name) . '?';
-	if($params == []){
-            return rtrim($nameRoute,'?');//если параметров нет, обрезаем "?" и возвращаем url
-	}
-    foreach ($params as $key => $value) {
-	$nameRoute .= $key . '=' . $value . '&';//добавляем указанные параметры к маршруту;
-    }
-    return rtrim($nameRoute, '&');//возвращаем url с параметрами, обрезая последний амперсант &;
+function memUrl($data = false)
+{
+    if ($data) {
+        $_SESSION['url'] = $data;
+    } else {
+        $url = $_SESSION['url'];
+        $_SESSION['url'] = '';
+        return $url;
+    }    
 }
 
-function limitStr($str, $n = 20) {
-    $array = explode(' ' , $str);
+function limitStr($str, $n = 20)
+{
+    $array = explode(' ', $str);
     $result = ''; // = пустая строка для добавления элементов масива пошагово.
     foreach ($array as $word) {
         $result .= ' ' . $word;
-        if(strlen($result) >= $n){
+        if (strlen($result) >= $n) {
             $string = $result . ' ...';
             break;
-        }else{
+        } else {
             $string = $result;
         }
     }
     return $string;
 }
 
-function debug($arr){
+function debug($arr)
+{
     echo '<pre>' . print_r($arr, true) . '</pre>';
 } 
 
-function redirect($str){
+function redirect($str)
+{
     header("Location:" . "$str" );
     exit;
 }
     
 function splashMessage($data = false, $class = 'info')
-    {
-        if($data) {
-            $_SESSION['error'] = $data;
-            $_SESSION['error_class'] = $class;
-        } else {
-            $message['data'] = $_SESSION['error'];
-            $message['class'] = $_SESSION['error_class'];
-            $_SESSION['error'] = '';
-            $_SESSION['error_class'] = '';
-            return $message;
-        }
+{
+    if ($data) {
+        $_SESSION['error'] = $data;
+        $_SESSION['error_class'] = $class;
+    } else {
+        $message['data'] = $_SESSION['error'];
+        $message['class'] = $_SESSION['error_class'];
+        $_SESSION['error'] = '';
+        $_SESSION['error_class'] = '';
+        return $message;
     }
-function oldData($data = false){
-    if($data){
+}
+
+function oldData($data = false)
+{
+    if ($data) {
         $_SESSION['form_data'] = $data;
     } else {
         $oldData = $_SESSION['form_data'];
         $_SESSION['form_data'] = '';
         return $oldData;
     }
-    
 }
 
 
