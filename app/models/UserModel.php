d5523ef5ad08e34a82cb393df29fa6bb0dc9b88d
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-30 12:58:31 +0200

    Add files via upload

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -2,13 +2,12 @@
 
 namespace app\models;
 
-
-
-class UserModel extends Model{
+class UserModel extends Model
+{
     protected $table = 'users';
 
-
-    public function read($email){
+    public function read($email)
+    {
         $stmt = $this->connect->prepare('SELECT * FROM users WHERE email = ?');
         $stmt->bind_param('s', $email);
         $stmt->execute();
@@ -16,16 +15,26 @@ class UserModel extends Model{
         $data = $result->fetch_assoc();
         return $data;
     }
-    public function create($username,$email,$password){
-        $stmt = $this->connect->prepare("INSERT INTO users (username,email, pass) VALUES (? ,? ,?)");
-        $stmt->bind_param('sss',$username, $email, $password);
+    
+    public function create($username, $email, $password)
+    {
+        $stmt = $this->connect->prepare("INSERT INTO users (username, email, pass) VALUES (?, ?, ?)");
+        $stmt->bind_param('sss', $username, $email, $password);
         $stmt->execute();
         $result = $stmt->insert_id;
         return $result;
     }
-    public function update($id, $username, $password, $avatar){
-        $stmt = $this->connect->prepare("UPDATE users SET username=?, pass =?, avatar =? WHERE id=$id");
-        $stmt->bind_param('sss',$username,$password,$avatar);
+    
+    public function update($id, $username, $password, $avatar = null)
+    {
+        if ($avatar !==null) {
+            $stmt = $this->connect->prepare("UPDATE users SET username=?, pass =?, avatar =? WHERE id=$id");
+            $stmt->bind_param('sss', $username, $password, $avatar);
+        } else {
+            $stmt = $this->connect->prepare("UPDATE users SET username=?, pass =? WHERE id=$id");
+            $stmt->bind_param('ss', $username, $password);
+            var_dump($stmt);
+        }
         $stmt->execute();
         $result = $stmt->insert_id;
         return $result;
