2bda38e64a09076c6b3925a4d4a2e7d0ffee2549
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-30 13:06:02 +0200

    Add files via upload

--- app/templates/UserController/edit.php
+++ app/templates/UserController/edit.php
@@ -4,23 +4,35 @@
         margin-top: 0.5em;" ><a href="/user"style="
                                                 color:#a7a49c; ">Профиль</a></h5>
 <div class="container">
-    <div class="account">
-	<h2 class="account-in">Редактировать пользователя:</h2>
-            <form action ="/user/edit" method="post" enctype="multipart/form-data">
-                <div> 	
-                    <span class="maill" style="padding-right: 60px;">Изменить имя:</span>
-                    <input type="text" name="username" value="<?= isset($_SESSION['form_data']['username'])? $_SESSION['form_data']['username']:$user['username'];?>"> 
-                </div>
-                <div> 	
-                    <span class="text">Изменить пароль:</span>
-                    <input type="password" name="pass" value=""> 
-                </div>
-                <div>
-                    <input type="file" name="path">
-                </div>
-                <div>
-                    <button type="submit" name="submit">Загрузить</button>
-                </div>
-            </form>
-    </div>
+  <div class="account">
+    <h2 class="account-in">Редактировать пользователя:</h2>
+      <form action ="/user/edit" method="post" enctype="multipart/form-data">
+        <div class="field"> 	
+          <span>Имя пользователя:</span>
+          <input type="text" name="username" value="<?= isset($_SESSION['form_data']['username'])? $_SESSION['form_data']['username']:$user['username'];?>"> 
+        </div>
+          <div class="field"> 	
+          <span>Новый пароль:</span>
+          <input type="password" name="pass" value=""> 
+        </div>
+        <div class="field"> 	
+          <span>Подтвердите новый пароль:</span>
+          <input type="password" name="pass_2" value=""> 
+        </div>
+        <div class="field">
+            <span>Загрузить аватар</span>
+            <input type="file" name="path">
+        </div>
+        <div class="container">
+          <h4 class="account-in">Подтвердить изменения:</h4>
+            <div class="field"> 	
+              <span>Введите текущий пароль:</span>
+              <input type="password" name="pass_user" value=""> 
+            </div>
+          <div class="field">
+            <button type="submit" name="submit">Обновить данные</button>
+          </div>  
+        </div>
+      </form>
+  </div>
 </div>
