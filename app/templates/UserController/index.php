3a0cac48265c1194ef118c0f71d0eb40ce82bdbc
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-30 15:28:52 +0200

    Add files via upload

--- app/templates/UserController/index.php
+++ app/templates/UserController/index.php
@@ -1,9 +1,13 @@
-<h3 align="center">Добро пожаловать <?= $user['username']?></h3>
+<h2 class="profile">Личные данные:</h2>
+<p><img src="images/avatar/<?php echo $user['avatar'];?>" class="profile-avatar" height="200" width="200"></p>
+<div class="field">
+    <p class="profile-user"><span>Имя:</span><?= $user['username']?></p>
+</div>
+<div class="field">
+    <p class="profile-user"><span>Ваш email:</span><?= $user['email']?></p>
+</div>
+<div class="edit-pro">
+    <a href="user/edit" class="edit-pro">Редактировать профиль</a>
+</div>
 
-<h5>Ваш email:<?= $user['email']?></h5>
-
-<p><img src="images/avatar/<?php echo $user['avatar'];?>" height="200" width="200"></p>
-<a href="user/edit" style="font-size:1em;
-                           font-style:italic;
-                           color:#a7a49c;">Редактировать профиль</a>
  
