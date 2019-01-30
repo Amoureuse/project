47b548c9886091744ba8880ce162a2323332a795
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-30 12:44:34 +0200

    Update AppController.php

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -3,16 +3,15 @@ namespace app\controllers;
 
 use project\base\Controller;
 
+Class AppController extends Controller
+{
+    
+    protected $model;
 
-Class AppController extends Controller {
-   
-
-    public function cookie(){
-        if(isset($_COOKIE['check'])){
-            return true;}
+    public function view($template, $data)
+    {
+        extract($data);
+        include(ROOT . '/app/templates/' . $template . '.php');
     }
-    public function view($template,$data){
-		extract($data);
-		include(ROOT . '/app/templates/' . $template . '.php');
-	}
+    
 }
