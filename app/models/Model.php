bb2e63dda3af40645ec7d7a3f4a1e523218a9c41
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-30 12:57:32 +0200

    Add files via upload

--- app/models/Model.php
+++ app/models/Model.php
@@ -1,24 +1,30 @@
 <?php
+
 namespace app\models;
+
 use project\Database;
-Class Model{
+
+Class Model
+{
     protected $connect;
     protected $table;
-    
 
-
-    public function __construct() {
+    public function __construct()
+    {
         $db = Database::getInstance();
-        $this->connect = $db->connection;
-        
+        $this->connect = $db->connection;  
     }
-    public function delete($id) {
+    
+    public function delete($id)
+    {
         $stmt = $this->connect->prepare("DELETE FROM $this->table WHERE id = ? ");
         $stmt->bind_param('i', $id);
         $result = $stmt->execute();
         return $result;
     }
-    public function readID($id){
+    
+    public function readID($id)
+    {
         $stmt = $this->connect->prepare("SELECT * FROM $this->table WHERE id = ? ");
         $stmt->bind_param('i', $id);
         $stmt->execute();
