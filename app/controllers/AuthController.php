bfb83aa90a09ec80842e8e6194366a8a7a171693
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-01-30 12:50:16 +0200

    Add files via upload

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,92 +1,112 @@
 <?php
 namespace app\controllers;
 
+use app\models\UserModel;
+use project\Auth;
 
-
-class AuthController extends AppController{
-    protected $model;
+class AuthController extends AppController
+{
     
-    public function __construct($route) {
+    public function __construct($route)
+    {
         parent::__construct($route);
-        $this->model = new \app\models\UserModel();
+        $this->model = new UserModel();
     }
 
-    public function login(){
-        if(!empty($_POST)){
+    public function login()
+    {
+        if (!empty($_POST)) {
             $email = $_POST['email'];
             $user = $this->model->read($email);
-            if(!$user){
+            if (!$user) {
                $error = "Пользователя с таким email не существует";
-               splashMessage($error);
+               splashMessage($error,'alert-danger');
                redirect('/login') ;
             }
-            if(password_verify($_POST['pass'], $user['pass'])){
-                \project\Auth::login($user['id']);
-                if(\project\Auth::isAdmin()){
+            if (password_verify($_POST['pass'], $user['pass'])) {
+                Auth::login($user['id']);
+                if (Auth::isAdmin()) {
                     redirect('/admin');
                 }
-                redirect('/');
-            }else{
+                $url = memUrl();
+                if (!empty ($url)) {
+                    redirect($url); 
+                } else {
+                    redirect('/'); 
+                }              
+            } else {
                 $oldData = [
                     'email' => $_POST['email']
                 ];
                 $error="Неверный пароль";
-                splashMessage($error,$oldData);
+                oldData($oldData);
+                splashMessage($error,'alert-danger');
                 redirect('/login'); 
             }
         }
-            $this->setMeta('Авторизация');
+        
+        $this->setMeta('Авторизация');
         
     }
-      public function register(){
-        if(!empty($_POST)){
+    
+    public function register()
+    {
+        if (!empty($_POST)) {
             $errors = $this->checkRegister();
-            if($errors !== true){
+            if ($errors !== true) {
                 $oldData = [
                     'email' => $_POST['email'],
                     'username'=>$_POST['username']
                 ];
-                splashMessage($errors[0],$oldData);
-            redirect('/register');
+                splashMessage($errors[0],'alert-danger');
+                oldData($oldData);
+                redirect('/register');
             }
             $data = $_POST;
-            $userId = $this->model->create($data);
-            \project\Auth::login($userId);
+            $username = $data['username'];
+            $email = $data['email'];
+            $password = password_hash($data['pass'], PASSWORD_DEFAULT);
+            $userId = $this->model->create($username, $email, $password);
+            Auth::login($userId);
             redirect('/');
-        }else{
-            $this->setMeta('Регистрация');
         }
+        
+        $this->setMeta('Регистрация');
+       
     }
     
-    public function logout(){
-        \project\Auth::logout();
+    public function logout()
+    {
+        Auth::logout();
         redirect('/');
     }
-    public function checkRegister(){
-        if(isset($_POST['submit_reg'])){ 
+    
+    public function checkRegister()
+    {
+        if (isset($_POST['submit'])) { 
             $email = $_POST['email'];
-            $user = $this->model->read(['email' => $email]);
+            $user = $this->model->read($email);
             
-            if(trim($_POST['username']) == '' || (!ctype_alnum($_POST['username']))){
+            if (trim($_POST['username']) == '' || (!ctype_alnum($_POST['username']))) {
                  $errors[] = 'Некорректный логин!';
             }
-            if(trim($_POST['email']) == ''){
+            if (trim($_POST['email']) == '') {
                  $errors[] = 'Введите email!';
             }
-            if(isset($user['email'])){
+            if (isset($user['email'])) {
                 $errors[] = 'Пользователь с таким email существует';
             }
-            if($_POST['pass'] == ''){
+            if ($_POST['pass'] == '') {
                  $errors[] = 'Введите пароль!';
             }
-            if($_POST['pass_2'] != $_POST['pass']){
+            if ($_POST['pass_2'] != $_POST['pass']) {
                  $errors[] = 'Пароли не совпадают!';
             }
         }
-        if(empty($errors)){
-                return true;
-        }else{
-                return $errors;
+        if (empty($errors)) {
+            return true;
+        } else {
+            return $errors;
         }
     }
     
