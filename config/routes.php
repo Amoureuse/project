88b05fab195782c22e3e8bd67f57be2601041f3f
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-02-22 19:15:05 +0200

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -5,18 +5,22 @@ Router::add('product/(?P<alias>[a-z0-9-]+)/?',  ['controller'=>'ProductControlle
 Router::add('category/(?P<alias>[a-z0-9-]+)/?',  ['controller'=>'CategoryController','action' =>'index']);
 
 Router::add('',                    ['controller'=>'HomeController','action' =>'main']);
+
 Router::add('cart',                ['controller'=>'CartController','action' =>'index']);
 Router::add('cart/add',            ['controller'=>'CartController','action' =>'add']);
 Router::add('cart/order',          ['controller'=>'CartController','action' =>'order']);
 Router::add('cart/delete',         ['controller'=>'CartController','action' =>'delete']);
+
 Router::add('show',                ['controller'=>'HomeController','action' =>'index']);
 Router::add('home/search',         ['controller'=>'HomeController','action' =>'search']);
-Router::add('currency/change',     ['controller'=>'CurrencyController','action' =>'change']);
+
 Router::add('login',               ['controller'=>'AuthController','action' =>'login']);
 Router::add('logout',              ['controller'=>'AuthController','action' =>'logout']);
 Router::add('register',            ['controller'=>'AuthController','action' =>'register']);
+
 Router::add('user',                ['controller'=>'UserController','action' =>'index']);
 Router::add('user/edit',           ['controller'=>'UserController','action' =>'edit']);
+
 Router::add('admin',               ['controller'=>'AdminController','action' =>'index',
     'prefix'=>'admin']);
 Router::add('admin/category',      ['controller'=>'CategoryController','action' =>'index',
