61c35ea71d4cde8ac1ea08b74cc6d3fc99263930
Amoureuse <45732721+Amoureuse@users.noreply.github.com>
2019-02-22 19:13:59 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ header("Content-type: text/html; charset=utf-8");
 require_once dirname(__DIR__) . '/config/init.php';
 require_once ROOT . '/config/env.php';
 require_once ROOT . '/config/config.php';
-//require_once ROOT . '/config/smtp.php';
+require_once ROOT . '/config/smtp.php';
 require_once ROOT . '/config/routes.php';
 require_once ROOT . '/core/function.php';
 
